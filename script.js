a6f78ed22950f210127e42721c947c2a4220eb2a
umipunkin <teddmandarinov@gmail.com>
2025-01-30 18:09:11 +0300

    hw-2. Домашняя работа 2

---
+++ script.js
@@ -0,0 +1,64 @@
+
+// 1-ое задание
+let a=10;
+// let a=20;
+alert(a);
+
+// 2-ое задание
+const release="Год выпуска первого iPhone - 2007";
+alert(release);
+
+// 3-е задание
+
+const nameCreator=`Брендан Эйх`;
+alert(nameCreator);
+
+// 4-ое задание
+
+let variableA=10;
+let variableB=2;
+let amount=variableA+variableB;
+alert(amount);
+let difference=variableA-variableB;
+alert(difference);
+let increase=variableA*variableB;
+alert(increase);
+let share=variableA/variableB;
+alert(share);
+
+
+// 5-ое задание
+
+let variableC=2**5;
+alert(variableC);
+
+// 6-ое задание
+
+let remains=9%2;
+alert(remains)
+
+// 7-ое задание
+let num = 1;
+num += 5;
+// num -= 3;
+// num *= 7;
+// num /= 3;
+// num += 1;
+// num -= 1;
+alert(num);
+
+// 8-ое задание
+let age=Number(prompt(`Сколько вам лет?`));
+alert(age);
+
+// // 9-ое задание
+const user = {
+    name: 'Умипункин',
+    age: 29,
+    "Likes pumpkins": true
+ }
+
+// 10-ое задание
+let userName=prompt(`Введите ваше имя`);
+let yourName=userName;
+alert(`Привет, ${yourName}`);
